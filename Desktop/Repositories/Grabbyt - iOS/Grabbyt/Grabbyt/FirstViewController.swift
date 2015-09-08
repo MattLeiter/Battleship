d4d4cd88213a494859ade16862d57f0be15d97f5
Hello <hello@straphq.com>
2015-09-08 02:27:01 -0400

    Buggy Initial Code

---
+++ Desktop/Repositories/Grabbyt - iOS/Grabbyt/Grabbyt/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Grabbyt
+//
+//  Created by Murtuza Kainan on 9/8/15.
+//  Copyright (c) 2015 Grabbyt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
